5389e1f7e4f82f476a490a4648a85ce742573137
Niko Virtala <nikovirtala@users.noreply.github.com>
2023-06-15 11:21:10 +0000

    fix: use amplify from alpha package

--- lib/cdk-amplify-console-stack.ts
+++ lib/cdk-amplify-console-stack.ts
@@ -1,24 +1,23 @@
-import { Stack, StackProps, SecretValue } from 'aws-cdk-lib';
+import { Stack, StackProps, SecretValue, aws_amplify, aws_codebuild } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as amplify from 'aws-cdk-lib/aws-amplify';
-import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as aws_amplify_alpha from '@aws-cdk/aws-amplify-alpha';
 
 export class AwsCdkAmplifyConsoleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const amplifyApp = new amplify.App(this, 'AmplifyConsoleDemo', {
-      sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
+    const amplifyApp = new aws_amplify_alpha.App(this, 'AmplifyConsoleDemo', {
+      sourceCodeProvider: new aws_amplify_alpha.GitHubSourceCodeProvider({
         owner: 'nikovirtala',
         repository: 'amplify-console-demo',
         oauthToken: SecretValue.secretsManager('github-access-token'),
       }),
       autoBranchCreation: {
         patterns: ['*'],
-        basicAuth: amplify.BasicAuth.fromGeneratedPassword('username'),
+        basicAuth: aws_amplify_alpha.BasicAuth.fromGeneratedPassword('username'),
       },
       autoBranchDeletion: true,
-      buildSpec: codebuild.BuildSpec.fromObject({
+      buildSpec: aws_codebuild.BuildSpec.fromObject({
         version: '1.0',
         frontend: {
           artifacts: {
@@ -31,7 +30,7 @@ export class AwsCdkAmplifyConsoleStack extends Stack {
 
     const master = amplifyApp.addBranch('master');
     const dev = amplifyApp.addBranch('dev', {
-      basicAuth: amplify.BasicAuth.fromGeneratedPassword('username'),
+      basicAuth: aws_amplify_alpha.BasicAuth.fromGeneratedPassword('username'),
     });
 
     const domain = amplifyApp.addDomain('amplified.host');
